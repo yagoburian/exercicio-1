08efd95946056fde1675de1f29d81922b11c32a5
Yago Burian <yago.burianlima@hotmail.com>
2021-04-26 13:56:06 -0300

    Exercicio 1

---
+++ src/Aplicativo.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import './Aplicativo.css';
+
+function Aplicativo() {
+
+  const [nome, setNome] = useState("")
+  const [telefone, setTelefone] = useState("")
+
+  return (
+    <div className="Aplicativo">
+      <form>
+        <div>
+          <p>
+            Digite seu nome:
+          </p>
+          <input
+            value={nome}
+            onChange={e => setNome(e.target.value)}
+            placeholder="Nome"
+          />
+        </div>
+        <div>
+          <p>
+            Digite seu telefone:
+          </p>
+          <input
+            value={telefone}
+            onChange={e => setTelefone(e.target.value)}
+            placeholder="Telefone"
+          />
+        </div>
+      </form>
+      <div className="Resultado">
+        <div className="Resultado_Total">
+          <p>{nome}</p>
+          <p>{telefone}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Aplicativo;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Aplicativo from './Aplicativo';
+ReactDOM.render(
+  <React.StrictMode>
+    <Aplicativo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
